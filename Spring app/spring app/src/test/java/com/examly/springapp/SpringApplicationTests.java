1f6e56f372a534fa123f193f79eee86f5ad902fb
Iamneo <techadmin@iamneo.ai>
2023-08-14 09:35:23 +0000

    Backup Commit

--- Spring app/spring app/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ Spring app/spring app/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -14,14 +14,59 @@ public class SpringApplicationTests {
 
     ChromeOptions chromeOptions = new ChromeOptions();
     WebDriver driver = null;
+    String bUrl = "http://iamneo.ai";
+		String facebookUrl = "https://www.facebook.com";
 
     @BeforeTest
     public void beforeTest() throws Exception
      {
    // replace seleniumhost and port with correct values
-        driver = new RemoteWebDriver(new URL("http://<seleniumhost:port>/"), chromeOptions);
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
         driver.manage().window().maximize();
     }
+    @Test(priority= 0)
+		public void oprn() {
+
+			driver.get(bUrl);
+
+			String titleOfThePage = driver.getTitle();
+			
+			Assert.assertEquals(titleOfThePage, "We are Hiring!");
+
+		}
+
+		@Test(priority= 1)
+		public void navigateCommands() {
+			driver.navigate().to(facebookUrl);
+		
+		}
+		@Test(priority= 2)
+		public void navigateCommandsb() {
+		
+			driver.navigate().back();
+		}
+		
+		@Test(priority= 3)
+		public void url1() {
+		
+			String currentUrl = driver.getCurrentUrl();
+
+			System.out.println("Current URL :: " + currentUrl);
+	}
+			
+		@Test(priority= 4)
+		public void url12() {
+			driver.navigate().forward();
+		}
+		
+		@Test(priority= 5)
+		public void url123() {
+			driver.navigate().refresh();
+
+	                
+		}
+	
+		
 
     @AfterTest
     public void afterTest() 
